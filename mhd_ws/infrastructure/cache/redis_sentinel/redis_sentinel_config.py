07ed991267b38d22273f86b0ef2ea6b6710f3249
oyurekten <ozgury@ebi.ac.uk>
2025-07-20 21:17:27 +0100

    redis update

--- mhd_ws/infrastructure/cache/redis_sentinel/redis_sentinel_config.py
+++ mhd_ws/infrastructure/cache/redis_sentinel/redis_sentinel_config.py
@@ -5,9 +5,13 @@ from pydantic import BaseModel
 from mhd_ws.infrastructure.cache.redis.redis_config import RedisService
 
 
+class RedisSentinelService(RedisService):
+    port: int = 26379
+
+
 class RedisSentinelConnection(BaseModel):
     master_name: str = "redis"
     password: str = ""
     db: int = 10
-    sentinel_services: List[RedisService] = []
+    sentinel_services: List[RedisSentinelService] = []
     socket_timeout: float = 0.5
